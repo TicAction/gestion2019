9607bc83338a7bf1eaf54b21b19b6c7af958b3df
Girard <ghislain.girard@032m1123-21370.invites.cssh.qc.ca>
2020-01-17 09:21:22 -0500

    17 janvier/page devoirs non dynamique

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,96 +4,66 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Devoirs et leçons de la semaine</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar navbar-dark bg-primary mb-5">
+        <a class="navbar-brand" href="#">Devoirs de la semaine</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+{{--            <ul class="navbar-nav">--}}
+{{--                <li class="nav-item active">--}}
+{{--                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>--}}
+{{--                </li>--}}
+{{--                <li class="nav-item">--}}
+{{--                    <a class="nav-link" href="#">Features</a>--}}
+{{--                </li>--}}
+{{--                <li class="nav-item">--}}
+{{--                    <a class="nav-link" href="#">Pricing</a>--}}
+{{--                </li>--}}
+{{--                <li class="nav-item">--}}
+{{--                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>--}}
+{{--                </li>--}}
+{{--            </ul>--}}
         </div>
+    </nav>
+    <h3 class="mb-2" style="text-align: center">Devoirs du 17 janvier au 23 janvier 2020</h3>
+
+    <table class="table table-striped d-lg-table">
+        <tr>
+            <th>Lecture : </th> <td>Chapitre 3 dans le livre "Zip sur terre"</td>
+        </tr>
+        <tr>
+            <th>Vocabulaire : </th> <td>Dans le carnet des savoirs page 62 unité 3.5</td>
+        </tr>
+        <tr>
+            <th>Grammaire : </th> <td>Dans le carnet des savoirs pages 15</td>
+        </tr>
+        <tr>
+            <th>Conjugaison : </th> <td>Dans le carnet des savoirs pages 31</td>
+        </tr>
+        <tr>
+            <th>Mathématique : </th> <td>Révision des table de multiplication</td>
+        </tr>
+        <tr>
+            <th>Communication oral : </th> <td>Les présentation commence lundi Je passe le : </td>
+        </tr>
+
+        <tr>
+            <th>Devoir de français : </th> <td>Terminer les pages jusqu'à 57 dans le manuel En Route.</td>
+        </tr>
+    </table>
+
+</div>
     </body>
 </html>
